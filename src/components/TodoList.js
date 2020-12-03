e17b086cdab5b7b1d8a28b65e43d9bc6714ab136
Nurbolot B <begimkuloff9889@gmail.com>
2020-12-04 10:20:39 +1300

    commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,64 @@
+import React, {useEffect,useState} from 'react';
+import TodoTitle from "./TodoTitle";
+import tasks from "../tasks";
+import TodoListItem from "./TodoListItem";
+import shortId from 'shortid'
+
+const TodoList = () => {
+    const [todos, setTodos] = useState([])
+    const [newTodo, setNewTodo] = useState('')
+    useEffect(() => {
+       setTodos(tasks)
+    },[])
+    const addTodo = () => {
+        const addedTodo = {
+            id:shortId.generate(),
+            title:newTodo,
+            done:false
+        }
+        setTodos([...todos,addedTodo])
+        setNewTodo('')
+    }
+    const deleteTodo = (deletedId) => {
+        const besides = todos.filter((el) => deletedId !== el.id)
+        setTodos(besides)
+    }
+    const updateTodo  = (update,id) => {
+        const updatedTodos = todos.map(el => el.id === id ? {...el, title: update} : el)
+        setTodos(updatedTodos)
+    }
+    const doneTodo  = (id) => {
+        const doneTodos = todos.map(el => el.id === id ? {...el, done: !el.done} : el)
+        setTodos(doneTodos)
+    }
+
+    return (
+        <div>
+            <TodoTitle todos={ todos }/>
+            <ul className="list-group mb-5">
+                {
+                    todos.map( todo => (
+                        <TodoListItem
+                            key={todo.id}
+                            todo={todo}
+                            deleteTodo={deleteTodo}
+                            updateTodo={updateTodo}
+                            doneTodo={doneTodo}
+                        />
+                    ))
+                }
+            </ul>
+            <input type="text"
+                   className="form-control"
+                   onChange={(e) => setNewTodo(e.target.value)}
+                    value={newTodo}
+            />
+            <button className="btn btn-secondary w-100 mt-2"
+                    onClick={addTodo}
+                    disabled={newTodo.length < 5}
+            >Add task</button>
+        </div>
+    );
+};
+
+export default TodoList;
---
+++ src/components/TodoListItem.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+
+const TodoListItem = ({todo, deleteTodo,updateTodo,doneTodo}) => {
+    const [editable, setEditable] = useState(false)
+    const [updated, setUpdated] = useState(todo.title)
+    const handleSave = (id) =>{
+        updateTodo(updated, id)
+        setEditable(false)
+    }
+    const isDoneStyle = {
+        textDecoration: 'line-through'
+    }
+    return (
+        <div>
+            <li className="list-group-item d-flex justify-content-between align-items-center">
+                { editable ? <input type="text" value={updated} onChange={e => setUpdated(e.target.value)}/> :
+                <h4 style={todo.done ? isDoneStyle : null} onClick={() => doneTodo(todo.id)}>{todo.title}</h4>
+                }
+                <div>
+                    <button
+                        className="btn btn-warning mr-2"
+                        onClick={() => editable ? handleSave(todo.id) : setEditable(true)}
+                    >{ editable ? 'Save' : 'Edit' }</button>
+                    <button
+                        className="btn btn-danger"
+                        onClick={ () => deleteTodo(todo.id) }
+                    >Del</button>
+                </div>
+            </li>
+
+        </div>
+    );
+};
+
+export default TodoListItem;
